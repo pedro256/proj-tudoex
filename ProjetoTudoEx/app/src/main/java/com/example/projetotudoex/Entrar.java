b4350a2b7845da84c94a5ac9e635ed7f75a6f8a0
Pedro Henrique Martins <pedroh93601@gmail.com>
2020-05-05 15:42:21 -0400

    ah

---
+++ ProjetoTudoEx/app/src/main/java/com/example/projetotudoex/Entrar.java
@@ -0,0 +1,14 @@
+package com.example.projetotudoex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Entrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrar);
+    }
+}
--- ProjetoTudoEx/app/src/main/java/com/example/projetotudoex/MainActivity.java
+++ ProjetoTudoEx/app/src/main/java/com/example/projetotudoex/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.projetotudoex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btcadastrar;
+    Button btEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btcadastrar = (Button) findViewById(R.id.btcadastrar);
+        btEntrar = (Button) findViewById(R.id.btentrar);
+
+        btcadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Chama = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(Chama);
+            }
+        });
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Chama = new Intent(MainActivity.this, Entrar.class);
+                startActivity(Chama);
+            }
+        });
     }
 }
