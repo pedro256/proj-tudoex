8f2a32fc1bfd322898594dd02fa3df0abad34135
3BIMAQ30 <3BIMAQ30@fundacaonokia.org>
2020-09-01 11:03:11 -0400

    sync-firebase

--- app/src/main/java/com/example/projetotudoex/CadastroActivity.java
+++ app/src/main/java/com/example/projetotudoex/CadastroActivity.java
@@ -2,7 +2,10 @@ package com.example.projetotudoex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -10,6 +13,18 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        Button btnVoltar = (Button) findViewById(R.id.btvoltar);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                proximaTela(v,MainActivity.class);
+            }
+        });
+    }
+    public void proximaTela(View view, Class tela){
+        Intent Ir = new Intent(getApplicationContext(), tela);
+        startActivity(Ir);
     }
 
     
--- app/src/main/java/com/example/projetotudoex/Entrar.java
+++ app/src/main/java/com/example/projetotudoex/Entrar.java
@@ -2,7 +2,10 @@ package com.example.projetotudoex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Entrar extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Entrar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrar);
+        Button btnVoltar = (Button)findViewById(R.id.btvoltar2);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                proximaTela(v,MainActivity.class);
+            }
+        });
+    }
+    public void proximaTela(View view,Class tela){
+        Intent Ir = new Intent(getApplicationContext(), tela);
+        startActivity(Ir);
     }
 }
